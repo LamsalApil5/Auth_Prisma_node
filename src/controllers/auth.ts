b548dc2a95da6b37a4eb7b5aeecf9e7b6c95d49e
LamsalApil5 <lamsalapil5@gmail.com>
2024-12-05 15:34:59 +0545

    First Prisma Work

---
+++ src/controllers/auth.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import { prismaClient } from "..";
+import { hashSync, compareSync } from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../secrets";
+import { BadRequestsException } from "../exceptions/bad-requests";
+import { ErrorCodes } from "../exceptions/root";
+// SignUp Handler
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (user) {
+    return next(new BadRequestsException('User not found!', ErrorCodes.USER_ALREADY_EXISTS))
+  }
+
+  user = await prismaClient.user.create({
+    data: {
+      email,
+      password: hashSync(password, 10),
+    },
+  });
+
+  res.json(user);
+};
+
+// Login Handler
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  let user = await prismaClient.user.findFirst({ where: { email } });
+  if (!user) {
+   return next(new BadRequestsException('User does not exists!', ErrorCodes.USER_NOT_FOUND));
+  
+  }
+
+  if (!compareSync(password, user.password)) res.json(user);
+  const token = jwt.sign(
+    {
+      userId: user.id,
+    },JWT_SECRET)
+
+    res.json({user, token})
+};
---
+++ src/exceptions/root.ts
@@ -0,0 +1,23 @@
+//Message, status code, error codes, error
+
+export class HttpException extends Error{
+    massage: string;
+    errorCode: any;
+    statusCode: number;
+    errors:ErrorCodes;
+
+    constructor(massage:string, errorCode:any, statusCode:number, error:any){
+        super(massage)
+        this.massage = massage
+        this.errorCode = errorCode
+        this.statusCode = statusCode
+        this.errors = error
+
+    }
+}
+
+export enum ErrorCodes{
+    USER_NOT_FOUND = 1001,
+    USER_ALREADY_EXISTS = 1002,
+    INCURRECT_PASSWORD = 1003,
+}
